37339f3a3e6589ae2e92296e957b1e42f20ec477
Matthew Alex <matthewalex@outlook.com>
2017-01-08 16:28:48 -0500

    sailfish: add kernel config and optimizations
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- sailfish/BoardConfig.mk
+++ sailfish/BoardConfig.mk
@@ -26,6 +26,9 @@ TARGET_2ND_CPU_VARIANT := krait
 
 ENABLE_CPUSETS := true
 
+STRICT_ALIASING := true
+POLLY_OPTS := true
+GRAPHITE_OPTS := true
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_KERNEL := false
 TARGET_NO_RECOVERY := true
@@ -106,10 +109,9 @@ BOARD_SECCOMP_POLICY += device/google/marlin/seccomp
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_GCC_VERSION_ARM64 := 6.x
 TARGET_KERNEL_SOURCE := kernel/google/marlin
-TARGET_KERNEL_CONFIG := pure_defconfig
+TARGET_KERNEL_CONFIG := sharkey_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 BOARD_KERNEL_BASE        := 0x80000000
