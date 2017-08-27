f4fb560c5cdcd1d1bce40fc3fa1d90f5ddb8ca30
spezi77 <spezi77@gmx.com>
2017-08-27 19:05:12 +0200

    TWRP Oreo patch [2/3] (gubacsek).
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- recovery/root/sbin/pulldecryptfiles.sh
+++ recovery/root/sbin/pulldecryptfiles.sh
@@ -4,24 +4,52 @@
 # This allows us to decrypt the device in recovery and still be
 # able to unmount /vendor when we are done.
 
-umount /vendor
-rm /vendor
-mkdir -p /vendor
-mount -t ext4 -o ro /dev/block/bootdevice/by-name/vendor_a /vendor
+SLOT=$(getprop ro.boot.slot_suffix)
 
-cp /vendor/lib64/libQSEEComAPI.so /sbin/libQSEEComAPI.so
-cp /vendor/lib64/hw/keystore.msm8996.so /sbin/keystore.msm8996.so
-cp /vendor/lib64/libdrmfs.so /sbin/libdrmfs.so
-cp /vendor/lib64/libdrmtime.so /sbin/libdrmtime.so
-cp /vendor/lib64/librpmb.so /sbin/librpmb.so
-cp /vendor/lib64/libssd.so /sbin/libssd.so
-cp /vendor/lib64/libdiag.so /sbin/libdiag.so
-cp /vendor/lib64/libkmcrypto.so /sbin/libkmcrypto.so
-cp /vendor/lib64/hw/gatekeeper.msm8996.so /sbin/gatekeeper.msm8996.so
 
-umount /vendor
+if [ "`mount|grep "/dev/block/bootdevice/by-name/system${SLOT} on /system_root"`" == "" ];then
+        mount -o rw,remount rootfs /
+        mkdir /system_root
+        mount -t ext4 -o ro /dev/block/bootdevice/by-name/system$SLOT /system_root
+else    
+        exit
+fi
 
-mkdir -p /vendor/lib64/hw
-cp /sbin/keystore.msm8996.so /vendor/lib64/hw/keystore.msm8996.so
-cp /sbin/gatekeeper.msm8996.so /vendor/lib64/hw/gatekeeper.msm8996.so
-cp /sbin/bootctrl.msm8996.so /vendor/lib64/hw/bootctrl.msm8996.so
+if [ "`mount|grep "/dev/block/bootdevice/by-name/system${SLOT} on /system "`" == "" ];then
+	mkdir -p /system
+        mount -o bind /system_root/system /system
+	mkdir /tmp/system
+	cp -r /system/lib64 /tmp/system/
+	cp -r /system/bin /tmp/system/
+	umount /system
+	umount /system_root
+	mkdir /system/lib64
+	mkdir /system/bin
+	mv /tmp/system/lib64/* /system/lib64/
+	mv /tmp/system/bin/* /system/bin/
+	rmdir /tmp/system/lib64 /tmp/system/bin /tmp/system
+fi
+
+if [ "`mount|grep "/dev/block/bootdevice/by-name/userdata on /data"`" == "" ];then
+	LD_LIBRARY_PATH=/system/lib64 /system/bin/e2fsck  -y -E journal_only /dev/block/bootdevice/by-name/userdata
+        LD_LIBRARY_PATH=/system/lib64 /system/bin/tune2fs -Q ^usrquota,^grpquota,^prjquota /dev/block/bootdevice/by-name/userdata
+        mount /dev/block/bootdevice/by-name/userdata /data
+fi
+
+if [ "`mount|grep "/dev/block/bootdevice/by-name/vendor${SLOT} on /vendor"`" == "" ];then
+	mkdir -p /vendor
+	mkdir -p /tmp/vendor
+        mount -t ext4 -o ro /dev/block/bootdevice/by-name/vendor_a /vendor
+
+	cp -r /vendor/lib64 /tmp/vendor/
+	cp -r /vendor/bin /tmp/vendor/
+
+	umount /vendor
+
+	mv /tmp/vendor/* /vendor/
+	rmdir /tmp/vendor
+fi
+
+
+setprop pulldecryptfiles.finished 1
+exit 0
